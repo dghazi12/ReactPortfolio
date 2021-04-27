0e50cdaddb1daee09e748aaeb31c347cda14311a
David Ghazi <davidaghazi@gmail.com>
2021-04-27 19:48:50 -0400

    updating resume

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,52 +1,59 @@
-import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
-import '../styling/Homepage.css';
-import profile from '../images/ProfilePicture/profile-picture.jpeg';
-import PDF from '../images/resume.pdf';
+import "../styling/Homepage.css";
+import profile from "../images/ProfilePicture/profile-picture.jpeg";
+import PDF from "../images/DavidGhazi_Resume.pdf";
 
 class Homepage extends Component {
-    render() {
-        return(
-            <div className="container">
-                <Grid className="Home-grid">
-                    <Cell col={12}>
-                            
-                        <img className='profile-picture' src={profile} alt="profile"></img>
+  render() {
+    return (
+      <div className="container">
+        <Grid className="Home-grid">
+          <Cell col={12}>
+            <img className="profile-picture" src={profile} alt="profile"></img>
 
-                        <div className="social-links">
-                                
-                            <a href="https://www.linkedin.com/in/davidaghazi/" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-linkedin socialResponsive" aria-hidden="true" />
-                            </a>
+            <div className="social-links">
+              <a
+                href="https://www.linkedin.com/in/davidaghazi/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i
+                  className="fa fa-linkedin socialResponsive"
+                  aria-hidden="true"
+                />
+              </a>
 
-                            <a href={PDF} rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-file" aria-hidden="true" />
-                            </a>
+              <a href={PDF} rel="noopener noreferrer" target="_blank">
+                <i className="fa fa-file" aria-hidden="true" />
+              </a>
 
-                            <a href="https://github.com/dghazi12" rel="noopener noreferrer" target="_blank">
-                                    <i className="fa fa-github" aria-hidden="true" />
-                            </a>
-
-                        </div>
-
-                        <div className="banner-text">
-                            <h1>Full Stack Web Developer</h1>
-                                
-                            <hr />
-
-                            <p> 
-                                HTML | CSS | React | Javascript | jQuery | Bootstrap
-                                    <br />  
-                                NodeJS | Express | MongoDB | mySQL | Heroku | Github
-                            </p>
-                        </div>
-
-                    </Cell>
-                </Grid>
+              <a
+                href="https://github.com/dghazi12"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fa fa-github" aria-hidden="true" />
+              </a>
             </div>
-        )
-    }
+
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+
+              <hr />
+
+              <p>
+                HTML | CSS | React | Javascript | jQuery | Bootstrap
+                <br />
+                NodeJS | Express | MongoDB | mySQL | Heroku | Github
+              </p>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Homepage;
